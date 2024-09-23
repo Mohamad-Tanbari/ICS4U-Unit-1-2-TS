2e6d0f9e0aa5119c3677068a5b733797f04b03b0
Mohamad Tanbari <mohamad.tanbari@mths.ca>
2024-09-23 14:05:28 +0000

    mass to joules

---
+++ main.ts
@@ -0,0 +1,35 @@
+/*
+ * This is a program that calculates joules produced by mass.
+ *
+ * @author Mohamad T
+ * @version 1.0
+ * @since 2024-9-23
+ */
+
+import { createPrompt } from 'bun-promptx'
+
+// variables
+const SPEED_OF_LIGHT = 2.998 * (10 ** 8)
+
+// input
+const massDictionary = createPrompt('Enter the mass of an object in kilograms: ')
+
+if (massDictionary.value === null) {
+  console.log('Invalid input; nulls not allowed.')
+} else {
+  // parse the value as a float
+  const massString = massDictionary.value
+  if (isNaN(parseFloat(massString))) {
+    console.log('Invalid input; not a number.')
+  } else {
+    // process
+    const mass = parseFloat(massString)
+    const energy = mass * (SPEED_OF_LIGHT ** 2)
+
+    // output
+    console.log(`${mass}kg of mass would produce ${energy}J of energy.`)
+  }
+}
+
+// done
+console.log('\nDone.')
